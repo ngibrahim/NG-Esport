77d6a05e093e30139baac5e1a3db774e3737a3b2
ngibrahim <40319290+ngibrahim@users.noreply.github.com>
2018-06-17 17:12:55 +0400

    Update bilgi.js

--- komutlar/bilgi.js
+++ komutlar/bilgi.js
@@ -13,7 +13,7 @@ exports.run = (client, message) => {
 	const pingozel = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Bot Hakkında: **Yapımcısı NGİbrahim---Küfür Engelleme---EĞlence KOmutları var**');
+    .setDescription('BotBilgi: **Yapımcısı NGİbrahim---Küfür Engelleme---EĞlence KOmutları var**');
     return message.author.sendEmbed(pingozel)
 };
 
