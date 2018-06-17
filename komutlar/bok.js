f77ab85bbf1e90da7b177a24542ba693181ddbb2
ngibrahim <40319290+ngibrahim@users.noreply.github.com>
2018-06-17 16:13:18 +0400

    Update bok.js

--- komutlar/bok.js
+++ komutlar/bok.js
@@ -1,1 +1,34 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
+exports.run = (client, message, params) => {
+	if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('**Eğlence Komutları Özel Mesajlarda Kullanılamaz!**')
+    return message.author.sendEmbed(ozelmesajuyari); }
+    if (message.channel.type !== 'dm') {
+      const sunucubilgi = new Discord.RichEmbed()
+    .setAuthor(message.author.username + 'IĞİY!')
+    .setColor(3447003)
+    .setTimestamp()
+    .setDescription('')
+		.setImage(`https://cdn.discordapp.com/emojis/457880315705294869.png?v=1`)
+    return message.channel.sendEmbed(sunucubilgi);
+    }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bok',
+  description: 'Bok Ye.',
+  usage: 'bok'
+};
