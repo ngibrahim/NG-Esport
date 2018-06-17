b4b61e643fb66ca8a8e4ace8fd3eaa8436e5f69e
ngibrahim <40319290+ngibrahim@users.noreply.github.com>
2018-06-17 16:05:03 +0400

    Update nsfw.js

--- komutlar/nsfw.js
+++ komutlar/nsfw.js
@@ -15,7 +15,7 @@ exports.run = (client, message, params) => {
     .setColor(3447003)
     .setTimestamp()
     .setDescription('')
-		.setImage(`https://www.google.az/imgres?imgurl=http%3A%2F%2Fthecontextofthings.com%2Fwp-content%2Fuploads%2F2017%2F08%2Frandom-sex-thing-i-wrote.jpg&imgrefurl=http%3A%2F%2Fthecontextofthings.com%2F2017%2F08%2F22%2Fnews-flash-bunch-sexual-history-will-transactional%2F&docid=YxfUA5I5rIKIrM&tbnid=96iTFpW6Sju6PM%3A&vet=10ahUKEwipn8XW0NrbAhWLXSwKHRrbA-4QMwgsKAkwCQ..i&w=960&h=640&client=opera&bih=219&biw=1113&q=sex&ved=0ahUKEwipn8XW0NrbAhWLXSwKHRrbA-4QMwgsKAkwCQ&iact=mrc&uact=8`)
+		.setImage(`https://upload.wikimedia.org/wikipedia/commons/2/24/Lateral_sex.jpg`)
     return message.channel.sendEmbed(sunucubilgi);
     }
 };
